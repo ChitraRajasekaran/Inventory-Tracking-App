d13900d6cba0d3d49ca5d6d6737dc3f9fe79f838
Chitra Rajasekaran <crinspire01@gmail.com>
2023-08-09 13:06:52 -0400

    Updated with Locations functionality

--- app.py
+++ app.py
@@ -18,21 +18,22 @@ app.app_context().push()
 class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=True)
 
     def __repr__(self):
         return f'<Product {self.id} {self.name}'
 class Location(db.Model):
     __tablename__ = 'locations'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=True)
+    
     def __repr__(self):
         return f'<Location {self.id} {self.name}'
 
 class Inventory(db.Model):
     __tablename__ = 'inventory'
     id = db.Column(db.Integer, primary_key = True)
-    product_id = db.Column(db.Integer,nullable = False)
+    product_id = db.Column(db.Integer,nullable = True)
     product_qty = db.Column(db.Integer,nullable = True)
     location_id = db.Column(db.Integer,nullable = True)
 
@@ -48,22 +49,23 @@ def create_inventory():
     headers = ['Products', 'Product_Qty','Location_Id']
     # products = db.session.query(Product.name,Product.id).join(Inventory,Inventory.product_id == Product.id)
     products = db.session.query(Product.name,Product.id).all()
-    return render_template('create_inventory.html',headers=headers,products=products)
+    locations = db.session.query(Location.name,Location.id).all()
+    return render_template('create_inventory.html',headers=headers,products=products,locations = locations)
 
 @app.route('/inventory/save', methods=['GET','POST'])
 def add_inventory():
-    headers = ['Product_id','Product_Qty','Location_Id']
+    headers = ['Product_id','Product_Qty']
     product_id = request.form.get('product_id','')
     product_qty = request.form.get('product_qty', '')
     location_id = request.form.get('location_id','')
-    
+
     if db.session.query(Inventory.query.filter(Inventory.product_id == product_id).exists()).scalar():
         inventory = db.session.query(Inventory).filter(Inventory.product_id == product_id).one()
         inventory.product_qty = product_qty
-        # inventory.location_id = location_id
+        inventory.location_id = location_id
         db.session.commit()
     else:
-        inventory = Inventory(product_id=product_id,product_qty=product_qty)
+        inventory = Inventory(product_id=product_id,product_qty=product_qty,location_id=location_id)
         db.session.add(inventory)
         db.session.commit()
     return redirect(url_for('inventory'))
@@ -71,16 +73,19 @@ def add_inventory():
 
 @app.route('/inventory/<int:id>/edit', methods = ['GET','POST'])
 def edit_inventory(id):
-    headers = ['Product_Name', 'Product_Qty']
+    headers = ['Product_Name', 'Product_Qty', 'Location_Name']
     products = db.session.query(Product.id,Product.name,Inventory.product_qty).join(Inventory,Inventory.product_id==Product.id).filter(Inventory.id == id)
-    return render_template('update_inventory.html',headers=headers, products = products, id=id)
+    locations = db.session.query(Location.name,Location.id).order_by("name").all()
+    return render_template('update_inventory.html',headers=headers, products = products, id=id, locations=locations)
 
 @app.route('/inventory/update', methods = ['GET','POST'])
 def update_inventory():
     id = request.form.get('pid','')
     product_qty = request.form.get('product_qty','')
+    location_id = request.form.get('location_id','')
     id_info = db.session.query(Inventory).filter(Inventory.id == id).one()
     id_info.product_qty = product_qty
+    id_info.location_id = location_id
     db.session.commit()
     return redirect(url_for('inventory'))
 
@@ -93,8 +98,10 @@ def delete_inventory(id):
 
 @app.route('/')
 def inventory():
-    headers = ['Id','Product_Name', 'Product_Qty','Location_Id']
-    products = db.session.query(Product.name,Product.id,Inventory.id,Inventory.product_qty,Inventory.location_id).join(Inventory,Inventory.product_id == Product.id).order_by(Inventory.id)
+    headers = ['Id','Product_Name', 'Product_Qty','Location_Name']
+    # only has products and inventory - products = db.session.query(Product.name,Product.id,Inventory.id,Inventory.product_qty,Inventory.location_id).join(Inventory,Inventory.product_id == Product.id).order_by(Inventory.id)
+    #the below has inventory,products and locations joined
+    products = db.session.query(Inventory.id.label('Inventory_id'),Inventory.product_qty,Inventory.location_id,Product.name.label('Product_name'),Product.id.label('Product_id'),Location.id.label('Location_id'),Location.name.label('Location_name')).join(Product,Inventory.product_id == Product.id).join(Location,Inventory.location_id == Location.id).order_by(Inventory.id)
     return render_template('index.html', headers=headers,tableData = products)
 
 
@@ -142,7 +149,8 @@ def delete_products(id):
 @app.route('/products')
 def products():
     headers = ['ProductName','Product_Qty','Actions']
-    return render_template('products.html', headers=headers,tableData = Product.query.order_by("name").all())
+    products = db.session.query(Product.id,Product.name,Inventory.product_qty).join(Inventory,Inventory.product_id == Product.id).order_by("name").all()
+    return render_template('products.html', headers=headers,tableData = products)
 
 
 
@@ -151,18 +159,28 @@ def products():
 # THE BELOW ROUTE NEED CHANGES ALTOGETHER TO MATCH WITH THE FUNCTIONALITY CHANGES
 #All location routes
 
-@app.route('/locations/create', methods=["POST"])
+@app.route('/locations/create', methods=['GET'])
+def create_locationss():
+    headers = ['LocationName','Actions']
+    return render_template('create_locations.html',headers=headers)
+
+@app.route('/locations/save', methods=['GET','POST'])
 def add_locations():
+    headers = ['LocationName','Actions']
     name = request.form.get('name', '')
-    locations = Location(name=name)
-    db.session.add(locations)
+    loc = Location(name=name)
+    db.session.add(loc)
+    db.session.commit()
+    id = db.one_or_404(db.select(Location.id).filter(Location.name == name))
+    inv = Inventory(location_id = id)
+    db.session.add(inv)
     db.session.commit()
     return redirect(url_for('locations'))
 
 @app.route('/locations/<int:id>/edit', methods = ['GET','POST'])
 def edit_locations(id):
     p_name = db.one_or_404(db.select(Location.name).filter(Location.id == id))
-    return render_template('edit_location.html',name = p_name, id=id)
+    return render_template('update_location.html',name = p_name, id=id)
 
 @app.route('/locations/update', methods = ['GET','POST'])
 def update_locations():
